0da389d395caa12eefeedf08d018a21fd4b360a8
Aman <162727802+verma2510@users.noreply.github.com>
2025-04-03 22:45:57 +0530

    experience profile updated

--- components/sections/Experience2.tsx
+++ components/sections/Experience2.tsx
@@ -1,94 +1,178 @@
-import Link from 'next/link'
-
+import Link from "next/link";
 
 export default function Experience2() {
-	return (
-		<>
-
-			<section id="portfolio" className="section-experience pt-5">
-				<div className="container">
-					<div className="rounded-3 border border-1 position-relative overflow-hidden">
-						<div className="box-linear-animation position-relative z-1">
-							<div className="p-lg-8 p-md-6 p-3 position-relative z-1">
-								<div className="d-flex align-items-center">
-									<svg className="text-primary-2 me-2" xmlns="http://www.w3.org/2000/svg" width={5} height={6} viewBox="0 0 5 6" fill="none">
-										<circle cx="2.5" cy={3} r="2.5" fill="#A8FF53" />
-									</svg>
-									<span className="text-linear-4 d-flex align-items-center"> Expericence </span>
-								</div>
-								<h3>
-									+12
-									<span className="text-300">years of </span>
-									passion
-									<span className="text-300">
-										for <br />
-										programming techniques
-									</span>
-								</h3>
-								<div className="row mt-5">
-									<div className="col-lg-4">
-										<div className="d-flex flex-column gap-2">
-											<Link href="#" className="technology border border-1 rounded-3 p-3">
-												<div className="d-flex align-items-center gap-2">
-													<img src="assets/imgs/home-page-2/experience/google.svg" alt="zelio" />
-													<div className="d-flex flex-column ms-2">
-														<h5 className="mb-1">Google</h5>
-														<span className="text-300">2018 - Present</span>
-													</div>
-												</div>
-											</Link>
-											<Link href="#" className="technology border border-1 rounded-3 p-3">
-												<div className="d-flex align-items-center gap-2">
-													<img src="assets/imgs/home-page-2/experience/twitter.svg" alt="zelio" />
-													<div className="d-flex flex-column ms-2">
-														<h5 className="mb-1">Twitter (X)</h5>
-														<span className="text-300">2012 - 2015</span>
-													</div>
-												</div>
-											</Link>
-											<Link href="#" className="technology border border-1 rounded-3 p-3">
-												<div className="d-flex align-items-center gap-2">
-													<img src="assets/imgs/home-page-2/experience/amazon.svg" alt="zelio" />
-													<div className="d-flex flex-column ms-2">
-														<h5 className="mb-1">Amazon</h5>
-														<span className="text-300">2018 - Present</span>
-													</div>
-												</div>
-											</Link>
-											<Link href="#" className="technology border border-1 rounded-3 p-3">
-												<div className="d-flex align-items-center gap-2">
-													<img src="assets/imgs/home-page-2/experience/payPal.svg" alt="zelio" />
-													<div className="d-flex flex-column ms-2">
-														<h5 className="mb-1">PayPal</h5>
-														<span className="text-300">2010 - 2012</span>
-													</div>
-												</div>
-											</Link>
-										</div>
-									</div>
-									<div className="col-lg-8 ps-lg-5 mt-5 mt-lg-0">
-										<h6 className="text-linear-4">Senior Software Engineer</h6>
-										<ul className="mt-4">
-											<li className="text-dark mb-3">Led development of scalable web applications, <span className="text-secondary-2">improving performance</span> and user experience for millions of users.</li>
-											<li className="text-dark mb-3">Implemented machine learning algorithms to enhance search functionality.</li>
-											<li className="text-dark mb-3">Collaborated with cross-functional teams to integrate new features seamlessly.</li>
-										</ul>
-										<div className="d-flex flex-wrap align-items-center gap-3 mt-7">
-											<Link href="#" className="text-300 border border-1 px-3 py-1">Python</Link>
-											<Link href="#" className="text-300 border border-1 px-3 py-1">TensorFlow</Link>
-											<Link href="#" className="text-300 border border-1 px-3 py-1">Angular</Link>
-											<Link href="#" className="text-300 border border-1 px-3 py-1">Kubernetes</Link>
-											<Link href="#" className="text-300 border border-1 px-3 py-1">GCP</Link>
-										</div>
-									</div>
-								</div>
-							</div>
-							<img className="position-absolute top-0 start-0 z-0" src="assets/imgs/home-page-2/services/bg.png" alt="zelio" />
-						</div>
-					</div>
-				</div>
-			</section>
-
-		</>
-	)
+  return (
+    <>
+      <section id="portfolio" className="section-experience pt-5">
+        <div className="container">
+          <div className="rounded-3 border border-1 position-relative overflow-hidden">
+            <div className="box-linear-animation position-relative z-1">
+              <div className="p-lg-8 p-md-6 p-3 position-relative z-1">
+                <div className="d-flex align-items-center">
+                  <svg
+                    className="text-primary-2 me-2"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={5}
+                    height={6}
+                    viewBox="0 0 5 6"
+                    fill="none"
+                  >
+                    <circle cx="2.5" cy={3} r="2.5" fill="#A8FF53" />
+                  </svg>
+                  <span className="text-linear-4 d-flex align-items-center">
+                    {" "}
+                    Expericence{" "}
+                  </span>
+                </div>
+                <h3>
+                  1+ <span className="text-300">years of </span>
+                  passion{" "}
+                  <span className="text-300">
+                    for <br />
+                    programming techniques
+                  </span>
+                </h3>
+                <div className="row mt-5">
+                  <div className="col-lg-4">
+                    <div className="d-flex flex-column gap-2">
+                      <Link
+                        href="#"
+                        className="technology border border-1 rounded-3 p-3"
+                      >
+                        <div className="d-flex align-items-center gap-2">
+                          <img
+                            src="https://nextgeninfratech.in/static/media/NextgenLogo.72c1f5ce548d335d66bd.png"
+                            alt="nextgen"
+                            className="nextgen-logo"
+                          />
+                          <div className="d-flex flex-column ms-2">
+                            <h5 className="mb-1">
+                              Nextgen Infratech Solutions LLP
+                            </h5>
+                            <span className="text-300">
+                              March, 2025 - Present
+                            </span>
+                            <span className="text-300">Internship</span>
+                          </div>
+                        </div>
+                      </Link>
+                      <Link
+                        href="#"
+                        className="technology border border-1 rounded-3 p-3"
+                      >
+                        <div className="d-flex align-items-center gap-2">
+                          <img
+                            src="http://accordo.ai/assets/logo-C9_SRjmA.svg"
+                            alt="accordo"
+                            className="accordo-logo"
+                          />
+                          <div className="d-flex flex-column ms-2">
+                            <h5 className="mb-1">Accordo.ai</h5>
+                            <span className="text-300">
+                              Aug, 2024 - Jan, 2025
+                            </span>
+                            <span className="text-300">Internship</span>
+                          </div>
+                        </div>
+                      </Link>
+                      {/* <Link
+                        href="#"
+                        className="technology border border-1 rounded-3 p-3"
+                      >
+                        <div className="d-flex align-items-center gap-2">
+                          <img
+                            src="assets/imgs/home-page-2/experience/amazon.svg"
+                            alt="zelio"
+                          />
+                          <div className="d-flex flex-column ms-2">
+                            <h5 className="mb-1">Amazon</h5>
+                            <span className="text-300">2018 - Present</span>
+                          </div>
+                        </div>
+                      </Link>
+                      <Link
+                        href="#"
+                        className="technology border border-1 rounded-3 p-3"
+                      >
+                        <div className="d-flex align-items-center gap-2">
+                          <img
+                            src="assets/imgs/home-page-2/experience/payPal.svg"
+                            alt="zelio"
+                          />
+                          <div className="d-flex flex-column ms-2">
+                            <h5 className="mb-1">PayPal</h5>
+                            <span className="text-300">2010 - 2012</span>
+                          </div>
+                        </div>
+                      </Link> */}
+                    </div>
+                  </div>
+                  <div className="col-lg-8 ps-lg-5 mt-5 mt-lg-0">
+                    <h6 className="text-linear-4">Full Stack Developer</h6>
+                    <ul className="mt-4">
+                      <li className="text-dark mb-3">
+                        Led development of scalable web applications,{" "}
+                        <span className="text-secondary-2">
+                          optimizing performance and enhancing user experience
+                        </span>{" "}
+                        for a growing user base.
+                      </li>
+                      <li className="text-dark mb-3">
+                        Implemented media-responsive design to enhance
+                        interactivity and user engagement.
+                      </li>
+                      <li className="text-dark mb-3">
+                        <span className="text-secondary-2">
+                          Collaborated with development teams on GitHub
+                        </span>{" "}
+                        to integrate new web features seamlessly, ensuring
+                        efficient <span className="text-secondary-2">version control </span> and deployment.
+                      </li>
+                    </ul>
+                    <div className="d-flex flex-wrap align-items-center gap-3 mt-7">
+                      <Link
+                        href="#"
+                        className="text-300 border border-1 px-3 py-1"
+                      >
+                        React
+                      </Link>
+                      <Link
+                        href="#"
+                        className="text-300 border border-1 px-3 py-1"
+                      >
+                        NodeJS
+                      </Link>
+                      <Link
+                        href="#"
+                        className="text-300 border border-1 px-3 py-1"
+                      >
+                        JavaScript
+                      </Link>
+                      <Link
+                        href="#"
+                        className="text-300 border border-1 px-3 py-1"
+                      >
+                        Tailwind CSS
+                      </Link>
+                      <Link
+                        href="#"
+                        className="text-300 border border-1 px-3 py-1"
+                      >
+                        GitHub
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <img
+                className="position-absolute top-0 start-0 z-0"
+                src="assets/imgs/home-page-2/services/bg.png"
+                alt="zelio"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
--- components/sections/Static2.tsx
+++ components/sections/Static2.tsx
@@ -15,7 +15,7 @@ export default function Static2() {
 										<div className="content mx-auto">
 											<i className="ri-shape-line text-primary-2" />
 											<h2 className="text-300 my-0 fs-50">
-												<CountUp className="odometer text-dark fw-medium" enableScrollSpy={true} end={12} />
+												<CountUp className="odometer text-dark fw-medium" enableScrollSpy={true} end={1} />
 												<span className="fs-50 text-300 mb-0">+</span>
 											</h2>
 											<p className="fs-6 mb-0 text-dark">Year Experience</p>
@@ -27,14 +27,14 @@ export default function Static2() {
 										<div className="content mx-auto">
 											<i className="ri-computer-line text-primary-2" />
 											<h2 className="text-300 my-0 fs-50">
-												<CountUp className="odometer text-dark fw-medium" enableScrollSpy={true} end={250} />
+												<CountUp className="odometer text-dark fw-medium" enableScrollSpy={true} end={10} />
 												<span className="fs-50 text-300 mb-0">+</span>
 											</h2>
 											<p className="fs-6 mb-0 text-dark">Projects Completed</p>
 										</div>
 									</div>
 								</div>
-								<div className="col-lg-auto col-md-6 text-center text-lg-start">
+								{/* <div className="col-lg-auto col-md-6 text-center text-lg-start">
 									<div className="counter-item-cover counter-item">
 										<div className="content mx-auto">
 											<i className="ri-service-line text-primary-2" />
@@ -45,8 +45,8 @@ export default function Static2() {
 											<p className="fs-6 mb-0 text-dark">Satisfied Clients</p>
 										</div>
 									</div>
-								</div>
-								<div className="col-lg-auto col-md-6 text-center text-lg-start">
+								</div> */}
+								{/* <div className="col-lg-auto col-md-6 text-center text-lg-start">
 									<div className="counter-item-cover counter-item">
 										<div className="content mx-auto">
 											<i className="ri-award-line text-primary-2" />
@@ -57,7 +57,7 @@ export default function Static2() {
 											<p className="fs-6 mb-0 text-dark">Awards Winner</p>
 										</div>
 									</div>
-								</div>
+								</div> */}
 							</div>
 						</div>
 						<div className="background position-absolute top-0 start-0 w-100 h-100 filter-invert" data-background="assets/imgs/home-page-2/static/bg.png" />
